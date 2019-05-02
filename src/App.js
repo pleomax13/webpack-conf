72f1e5483b882b71d29401fc7b8656121353c525
pleomax13 <43319882+pleomax13@users.noreply.github.com>
2019-05-02 14:55:26 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
+import Home from './components/Home';
 
 class App extends React.Component {
     render() {
         return(
             <div className = 'App'>
                 App
+                <Home />
                 <img src = {logo} />
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
-module.exports = {
+module.exports = (env, argv) => ({
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[hash].main.js'
+        path: path.resolve(__dirname, 'build'),
+        chunkFilename: 
+            argv.mode === 'production' ? 'js/[chunkhash].[id].chunk.js' : 'js/[id].chunk.js',
+        filename: 
+            argv.mode === 'production' ? 'js/[name].[chunkhash].js' : 'js/[name].js'
     },
+    devtool: 'source-map',
     plugins: [
+        argv.mode === 'production' ? new CleanWebpackPlugin() : new CleanWebpackPlugin({dry: true}), 
         new HtmlWebpackPlugin({
-            template: 'public/index.html'
+            template: 'public/index.html',
+            filename: 'index.html',
+            hash: true
         }),
-        new MiniCssExtractPlugin({filename: '[hash].style.css'})
+        new MiniCssExtractPlugin({
+            filename: 
+                argv.mode === 'production' ? 'css/[name].[contenthash].css' : 'css/[name].css'
+        }),  
+        new WebpackMd5Hash(),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'public'),
@@ -32,14 +46,83 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
-                use:  ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+                test: /\.(css|scss)$/,
+                use:  [
+                    'style-loader', 
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader']
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'media'
+                    }
+                }
             },
         ]
     }
 
-}
+});
+/*
+var config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[hash].main.js'
+    },
+  };
+  
+  module.exports = (env, argv) => {
+  
+        config.module = {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use:  ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: ['file-loader']
+                },
+            ]
+        };
+
+        config.devServer = {
+            contentBase: path.join(__dirname, 'public'),
+            compress: true,
+            port: 3000,
+            watchContentBase: true,
+            progress: true,
+            open: true
+        };
+
+        config.plugins =[
+            new HtmlWebpackPlugin({
+                template: 'public/index.html'
+            }),
+            new MiniCssExtractPlugin({filename: '[hash].style.css'}),
+            argv.mode === 'production' ? new MinifyPlugin({}, {comments: false}) : null    
+        ]
+  
+    if (argv.mode === 'production') {
+      //...
+    }
+  
+    return config;
+  };*/
