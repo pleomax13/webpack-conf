ee4dc3c9cf01ace7b4d53ad5bb340e3376dd5812
pleomax13 <43319882+pleomax13@users.noreply.github.com>
2019-05-03 15:31:43 +0300

    Updates

---
+++ precache-manifest.7c5ee81d9625f46326d43b65be39c6a1.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362b1f60da476fcd9424",
+    "url": "css/main.4ba0392f.css"
+  },
+  {
+    "revision": "e965f54abd464b0163776ed7f93589ed",
+    "url": "index.html"
+  },
+  {
+    "revision": "362b1f60da476fcd9424",
+    "url": "js/main.362b1f60.chunk.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e980",
+    "url": "js/runtime~main.d41d8cd9.js"
+  },
+  {
+    "revision": "ca9956652aa109c343f8",
+    "url": "js/vendors~main.ca995665.chunk.js"
+  },
+  {
+    "url": "media/5d5d9eefa31e5e13a6610d9fa7a283bb.svg"
+  }
+]);
